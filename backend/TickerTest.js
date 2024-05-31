24df9f6ed213df490cf4055a5027fafffab6fc6d
0szigeth <justin.szigethy@studium.uni-hamburg.de>
2024-05-31 16:17:00 +0200

    added some backend stuff, fixed styling

---
+++ backend/TickerTest.js
@@ -0,0 +1,27 @@
+import yahooFinance from "yahoo-finance2";
+import fs from "fs";
+
+async function ticker() {
+  const query = "AAPL";
+  const queryOptions = {
+    period1: "2023-05-07",
+    period2: "2024-05-15",
+    interval: "1d",
+  };
+  const result = await yahooFinance.chart(query, queryOptions);
+
+  let jsonData = JSON.stringify(result.quotes);
+  console.log(jsonData);
+
+  fs.writeFile(
+    "../frontend/my-react-app/src/data/charts.json",
+    jsonData,
+    function (err) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+}
+
+console.log(ticker());
