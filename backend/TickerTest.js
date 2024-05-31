6bbc96072f74c23001a199c6c4a09c59aa4badc7
Romolusgit <remoerchinger@hotmail.de>
2024-05-31 18:21:55 +0200

    testing branch

--- backend/TickerTest.js
+++ backend/TickerTest.js
@@ -1,8 +1,10 @@
 const yahooFinance = require("yahoo-finance2").default;
 const fs = require("fs");
 
-async function ticker() {
-  const query = "AAPL";
+//ticker = input("Gib ticker ein: ")
+
+async function ticker(ticker) {
+  const query = ticker;
   const queryOptions = {
     period1: "2023-05-07",
     period2: "2024-05-15",
